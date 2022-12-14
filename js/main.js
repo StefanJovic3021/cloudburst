67c3ed047abe5c1927c5838d223b5ccf9ff4b341
StefanJovic3021 <95912605+StefanJovic3021@users.noreply.github.com>
2022-12-14 23:23:11 +0100

    Update fix

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const navBarText = ["Home", "About us", "Our services", "Contact us", "About aut
 let navBarCode = "<ul>";
 for (let i = 0; i < navBarLinks.length; i++)
 {
-    if (document.location.pathname == navBarLinks[i] || document.location.pathname == "/cloudburst/")
+    if (document.location.pathname == navBarLinks[i])
     {
         navBarCode += `<li><a href="${navBarLinks[i]}" class="active"><p>${navBarText[i]}</p></a></li>`;
     }
